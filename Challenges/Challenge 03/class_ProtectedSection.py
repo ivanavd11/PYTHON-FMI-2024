ff1517cab0780457d03ca0f84569c3e018128074
Ivana Dimitrova <147918932+ivanavd11@users.noreply.github.com>
2024-11-24 17:26:27 +0200

    Add files via upload

---
+++ Challenges/Challenge 03/class_ProtectedSection.py
@@ -0,0 +1,23 @@
+class ProtectedSection:
+    def __init__(self, log=(), suppress=()):
+        self.log = log
+        self.suppress = suppress
+        self.exception = None
+
+    def __enter__(self):
+        self.exception = None
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is None:
+            return True
+
+        if isinstance(exc_val, self.log):
+            self.exception = exc_val
+            return True
+
+        if isinstance(exc_val, self.suppress):
+            return True
+
+        return False 
+
